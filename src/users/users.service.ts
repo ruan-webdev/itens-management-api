dc24b64659c41201b787cd03d33ff7ee7b76ab9e
Ruan Campos <ruancamposdemarcos@gmail.com>
2024-10-11 19:32:24 -0300

    feat: add password cryptography bcrtypt

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/database/prisma.service';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
   async create(createUserDto: CreateUserDto) {
+    const encryptedPassword = await hash(createUserDto.password, 10);
+
     const userCreated = this.prisma.user.create({
-      data: createUserDto,
+      data: { ...createUserDto, password: encryptedPassword },
     });
 
     return userCreated;
